e86b7ca9b7cc7b1f919332790ca1940a44ec7c89
elenayran <36763284+elenayran@users.noreply.github.com>
2021-06-09 17:50:51 +0300

    TO-DO-CASE4#F1#
    Gönderi takip numarası, bilgi servisleri, teslimat süresi, fiyat hesaplama bilgileri

--- kargotakip_giristasarım.cs
+++ kargotakip_giristasarım.cs
@@ -57,3 +57,86 @@ namespace KargoTakip
             this.label2.Size = new System.Drawing.Size(104, 13);
             this.label2.TabIndex = 3;
             this.label2.Text = "şeklinde giriş yapınız.";
+
+             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.25F);
+            this.label1.Location = new System.Drawing.Point(4, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(174, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Gönderi takip numarası için 100001 ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(20, 40);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(113, 23);
+            this.textBox1.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::KargoTakip.Properties.Resources.search_icon;
+            this.pictureBox1.Location = new System.Drawing.Point(139, 31);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(55, 42);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Location = new System.Drawing.Point(349, 157);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(256, 135);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Bilgi Servisleri";
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.ForeColor = System.Drawing.Color.Blue;
+            this.button1.Image = global::KargoTakip.Properties.Resources.zaman;
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button1.Location = new System.Drawing.Point(20, 28);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(99, 84);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Teslimat Süreleri";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.ForeColor = System.Drawing.Color.Blue;
+            this.button2.Image = global::KargoTakip.Properties.Resources.many1;
+            this.button2.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button2.Location = new System.Drawing.Point(139, 28);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(98, 84);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Fiyat Hesaplama";
+            this.button2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.groupBox3);
+            this.panel1.Location = new System.Drawing.Point(27, 315);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(690, 215);
+            this.panel1.TabIndex = 4;
+
+
+            #endregion
+}   }   }            
