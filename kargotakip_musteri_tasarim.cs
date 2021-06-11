2ae466725686aeb7aa7563a2f50c8fd3f121c8c8
gizemirmak <gizemirmak16@gmail.com>
2021-06-11 09:46:40 +0300

    TO-DO-CASE12#F1
    Ödeme tipi, Gönderilen tarih, Adrese teslim, Şubeye teslim, Teslim tipi tasarımı ekran kodu

--- kargotakip_musteri_tasarim.cs
+++ kargotakip_musteri_tasarim.cs
@@ -26,6 +26,10 @@ namespace KargoTakip
             this.label4 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
             // 
             // groupBox1
             // 
@@ -92,3 +96,42 @@ namespace KargoTakip
             this.comboBox3.Name = "comboBox3";
             this.comboBox3.Size = new System.Drawing.Size(103, 26);
             this.comboBox3.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 122);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 18);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Ödeme Tipi";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(111, 18);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Gönderilen Tarih";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Adrese Teslim",
+            "Şubeye Teslim"});
+            this.comboBox1.Location = new System.Drawing.Point(113, 43);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(103, 26);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Teslim Tipi";
