832a7213b48e8f4325ff74709f10f26d65730c44
elenayran <36763284+elenayran@users.noreply.github.com>
2021-06-14 20:24:33 +0300

    TO-DO-CASE16#F1#
    yönetici ana sayfası, içerisinde şube, personel, taşıt, fatura bilgileri bulunan butonlardır.

---
+++ Kargotakip_yöneticianasayfa.cs
@@ -0,0 +1,108 @@
+namespace KargoTakip
+{
+    partial class yönetici
+    {
+      
+        private System.ComponentModel.IContainer components = null;
+
+      
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        
+        private void InitializeComponent()
+        {
+
+           this.button1 = new System.Windows.Forms.Button();
+           this.button2 = new System.Windows.Forms.Button();
+           this.button3 = new System.Windows.Forms.Button();
+           this.button4 = new System.Windows.Forms.Button();
+
+// 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Verdana", 8.25F);
+            this.button1.Image = global::KargoTakip.Properties.Resources.chat_icon;
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button1.Location = new System.Drawing.Point(47, 124);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(119, 82);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "ŞUBE";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // 
+            // button2
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Verdana", 8.25F);
+            this.button1.Image = global::KargoTakip.Properties.Resources.chat_icon;
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button1.Location = new System.Drawing.Point(47, 124);
+            this.button1.Name = "button2";
+            this.button1.Size = new System.Drawing.Size(119, 82);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "PERSONEL";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // // 
+            // button3
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Verdana", 8.25F);
+            this.button1.Image = global::KargoTakip.Properties.Resources.chat_icon;
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button1.Location = new System.Drawing.Point(47, 124);
+            this.button1.Name = "button3";
+            this.button1.Size = new System.Drawing.Size(119, 82);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "TAŞIT";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // // 
+            // button4
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Verdana", 8.25F);
+            this.button1.Image = global::KargoTakip.Properties.Resources.chat_icon;
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button1.Location = new System.Drawing.Point(47, 124);
+            this.button1.Name = "button4";
+            this.button1.Size = new System.Drawing.Size(119, 82);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "FATURA";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+
+
+
+
+
+
+
+
+            
+        }
+
+        #endregion
+
+           private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }}
